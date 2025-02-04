3060b14e5ca1e9b1f05ed0e327725340bb4770a2
Stepan Osadshy <s.osadshy@horo.school>
2025-02-04 20:06:43 +0300

    add main page fixture

--- tests/specs/unauthorizaded/mainPage.spec.ts
+++ tests/specs/unauthorizaded/mainPage.spec.ts
@@ -1,21 +1,12 @@
-import test from '@playwright/test';
+import { test, expect } from '../../fixtures/fixtures';
 import { MainPage } from '../../pages/MainPage';
 
-test('Проверка доступности элементов хедера', async ({ page }) => {
-  const mainPage = new MainPage(page);
-  await mainPage.open();
-  await mainPage.closeCookiesAlert();
+test('Проверка доступности элементов хедера', async ({ mainPage }) => {
   await mainPage.headerHasCorrectAriaSnapshot();
 });
-test('Проверка доступности элементов табов категорий', async ({ page }) => {
-  const mainPage = new MainPage(page);
-  await mainPage.open();
-  await mainPage.closeCookiesAlert();
+test('Проверка доступности элементов табов категорий', async ({ mainPage }) => {
   await mainPage.categoriesTabsHasCorrectAriaSnapshot();
 });
-test('Проверка доступности элементов бокового меню', async ({ page }) => {
-  const mainPage = new MainPage(page);
-  await mainPage.open();
-  await mainPage.closeCookiesAlert();
+test('Проверка доступности элементов бокового меню', async ({ mainPage }) => {
   await mainPage.menuHasCorrectAriaSnapshot();
 });
