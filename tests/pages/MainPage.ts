8bb0a257f61efd77581a87c8a5880df3f5ccd5c6
Stepan Osadshy <s.osadshy@horo.school>
2025-02-09 12:20:48 +0300

    main page tests

--- tests/pages/MainPage.ts
+++ tests/pages/MainPage.ts
@@ -4,6 +4,13 @@ export class MainPage extends BasePage {
   private readonly headerLocator: Locator;
   private readonly categoriesTabsLocator: Locator;
   private readonly menuLocator: Locator;
+  private readonly headerAddButtonLocator: Locator;
+  private readonly headerNotificationsButtonLocator: Locator;
+  private readonly headerLoginButtonLocator: Locator;
+  private readonly headerAddButtonPopupListLocator: Locator;
+  private readonly headerNotificationsPopupLocator: Locator;
+  private readonly authorizationModalLocator: Locator;
+  private readonly switchToRegistrationModalButtonLocator: Locator;
 
   constructor(page: Page) {
     super(page);
@@ -12,17 +19,71 @@ export class MainPage extends BasePage {
       hasText: /^ГлавнаяФильмыСериалыТелешоуСпортБлогерыНовостиМузыкаПодкастыДетямТВ онлайн$/,
     });
     this.menuLocator = this.page.getByLabel('Облегченная панель навигации');
+    this.headerAddButtonLocator = this.page.getByRole('button', { name: 'Добавить' });
+    this.headerNotificationsButtonLocator = this.page.getByRole('button', { name: 'Уведомления' });
+    this.headerLoginButtonLocator = this.page.getByRole('button', { name: 'Вход и регистрация' });
+    this.headerAddButtonPopupListLocator = this.page.locator(
+      '.wdp-header-right-module__uploader ul',
+    );
+    this.headerNotificationsPopupLocator = this.page.locator(
+      '.wdp-notifications-popup-module__wrapper',
+    );
+    this.authorizationModalLocator = this.page
+      .locator('iframe[title="Multipass"]')
+      .contentFrame()
+      .locator('div[role="form"]');
+    this.switchToRegistrationModalButtonLocator = this.page
+      .locator('iframe[title="Multipass"]')
+      .contentFrame()
+      .locator('div[role="form"]')
+      .getByRole('button', { name: 'Зарегистрироваться' });
   }
   async open() {
     this.page.goto('https://rutube.ru/');
   }
   async headerHasCorrectAriaSnapshot() {
-    await expect(this.headerLocator).toMatchAriaSnapshot();
+    await expect(this.headerLocator).toMatchAriaSnapshot({ name: 'headerAriaShapshot.yml' });
   }
   async categoriesTabsHasCorrectAriaSnapshot() {
-    await expect(this.categoriesTabsLocator).toMatchAriaSnapshot();
+    await expect(this.categoriesTabsLocator).toMatchAriaSnapshot({
+      name: 'categoriesTabsShapshot.yml',
+    });
   }
   async menuHasCorrectAriaSnapshot() {
-    await expect(this.menuLocator).toMatchAriaSnapshot();
+    await expect(this.menuLocator).toMatchAriaSnapshot({
+      name: 'menuShapshot.yml',
+    });
+  }
+  async openAddPopupList() {
+    this.headerAddButtonLocator.click();
+  }
+  async openNotificationsPopup() {
+    this.headerNotificationsButtonLocator.click();
+  }
+  async openAuthorizationModal() {
+    this.headerLoginButtonLocator.click();
+  }
+  async switchToRegistrationModal() {
+    this.switchToRegistrationModalButtonLocator.click();
+  }
+  async addPopupListHasCorrectAriaSnapshot() {
+    await expect(this.headerAddButtonPopupListLocator).toMatchAriaSnapshot({
+      name: 'addButtonPopupList.yml',
+    });
+  }
+  async notificationsPopupHasCorrectAriaSnapshot() {
+    await expect(this.headerNotificationsPopupLocator).toMatchAriaSnapshot({
+      name: 'notificationsPopup.yml',
+    });
+  }
+  async authorizationModalHasCorrectAriaSnapshot() {
+    await expect(this.authorizationModalLocator).toMatchAriaSnapshot({
+      name: 'authorizationModal.yml',
+    });
+  }
+  async registrationModalHasCorrectAriaSnapshot() {
+    await expect(this.authorizationModalLocator).toMatchAriaSnapshot({
+      name: 'registrationModal.yml',
+    });
   }
 }
