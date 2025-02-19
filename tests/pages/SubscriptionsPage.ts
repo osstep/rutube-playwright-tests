47ffa1c21d978f689c3a72cb4e4e70925b6549d0
Stepan Osadshy <s.osadshy@horo.school>
2025-02-19 08:19:44 +0300

    subscriptions page tests

---
+++ tests/pages/SubscriptionsPage.ts
@@ -0,0 +1,18 @@
+import { Locator, Page } from '@playwright/test';
+import { BasePage } from './BasePage';
+
+export class SubscriptionsPage extends BasePage {
+  private readonly contentPageLocator: Locator;
+
+  constructor(page: Page) {
+    super(page);
+
+    this.contentPageLocator = this.page.locator('.application-module__content');
+  }
+  async open() {
+    await this.page.goto('https://rutube.ru/my/subscriptions/');
+  }
+  async contentHasCorrectAriaSnapsot() {
+    await this.checkAriaSnapshot(this.contentPageLocator, 'contentAriaSnapshot.yml');
+  }
+}
